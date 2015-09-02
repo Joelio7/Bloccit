ae5d93c498b64a4f7cdade54c2fe1acac9522b9a
Your Name <joe.scalera@gmail.com>
2015-09-02 05:40:01 -0700

    crud 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     @posts.each_with_index do |post, index|
-  if index % 5 == 4 
+  if index % 5 == 4
     post.update_attributes(title: 'CENSORED')
   end
 end
@@ -34,5 +34,34 @@ end
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post] [:title]
+    @post.body = params[:post] [:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+     @post = Post.find(params[:id])
+
+ # #8
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to posts_path
+     else
+       flash[:error] = "There was an error deleting the post."
+       render :show
+     end
+   end
+
 end
