d83d736fd54f3f340f63f5881cfe2e8cb8d3a9a8
Your Name <joe.scalera@gmail.com>
2015-09-02 02:03:28 -0700

    advertisement

--- app/controllers/advertisement_controller.rb
+++ app/controllers/advertisement_controller.rb
@@ -4,7 +4,7 @@ class AdvertisementController < ApplicationController
   end
 
   def show
-    @advertisement = Advertisement.find(params[:id])
+    
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'contact' => 'welcome#contact'
   get 'about' => 'welcome#about'
   resources :posts
-  resources :advertisements
+  resources :advertisement
   root to: 'welcome#index'
 end
