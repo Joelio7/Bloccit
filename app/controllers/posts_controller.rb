28696a69b70b5e6890b37ec7c8d97821dfb33aab
Your Name <joe.scalera@gmail.com>
2015-09-05 20:27:37 -0700

    topics and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,65 +1,60 @@
 class PostsController < ApplicationController
+     def show
+       @post = Post.find(params[:id])
+     end
 
+     def new
+       @topic = Topic.find(params[:topic_id])
+       @post = Post.new
+     end
 
+     def create
+       @post = Post.new
+       @post.title = params[:post][:title]
+       @post.body = params[:post][:body]
+       @topic = Topic.find(params[:topic_id])
+   # #35
+       @post.topic = @topic
 
-  def show
-    @post = Post.find(params[:id])
-  end
+       if @post.save
+         flash[:notice] = "Post was saved."
+   # #36
+         redirect_to [@topic, @post]
+       else
+         flash[:error] = "There was an error saving the post. Please try again."
+         render :new
+       end
+     end
 
-  def new
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.new
-  end
+     def edit
+       @post = Post.find(params[:id])
+     end
 
-  def create
+     def update
+       @post = Post.find(params[:id])
+       @post.title = params[:post][:title]
+       @post.body = params[:post][:body]
 
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
-    @topic = Topic.fin(params[:topic_id])
+       if @post.save
+         flash[:notice] = "Post was updated."
+   # #37
+         redirect_to [@post.topic, @post]
+       else
+         flash[:error] = "There was an error saving the post. Please try again."
+         render :edit
+       end
+     end
 
-    @post.topic = @topic
+     def destroy
+       @post = Post.find(params[:id])
 
-    if @post.save
-
-      flash[:notice] = "Post was saved."
-      redirect_to [@topic, @post]
-    else
-
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :new
-    end
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
-
-  def update
-    @post = Post.find(params[:id])
-    @post.title = params[:post] [:title]
-    @post.body = params[:post] [:body]
-
-    if @post.save
-      flash[:notice] = "Post was updated."
-      redirect_to [@post.topic, @post]
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
-    end
-  end
-
-  def destroy
-     @post = Post.find(params[:id])
-
- # #8
-     if @post.destroy
-       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-       redirect_to @post.topic
-     else
-       flash[:error] = "There was an error deleting the post."
-       render :show
+       if @post.destroy
+         flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+   # #38
+          redirect_to @post.topic
+       else
+         flash[:error] = "There was an error deleting the post."
+         render :show
+       end
      end
    end
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   get 'contact' => 'welcome#contact'
   get 'about' => 'welcome#about'
   resources :topics do
-    resources :posts, expect: [:index]
-    end
-  resources :advertisement
-  root to: 'welcome#index'
-end
+ # #34
+     resources :posts, except: [:index]
+   end
+
+   get 'about' => 'welcome#about'
+   root to: 'welcome#index'
+ end
