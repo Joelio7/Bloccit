6e1e714aa71007f6212868d2aefefba72cb6e438
Your Name <joe.scalera@gmail.com>
2015-09-09 01:23:03 -0700

    advertisement final

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -24,18 +24,18 @@ RSpec.describe AdvertisementsController, type: :controller do
      expect(response).to have_http_status(:success)
    end
  end
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-  #
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+   describe "GET #new" do
+     it "returns http success" do
+       get :new
+       expect(response).to have_http_status(:success)
+     end
+   end
+
+   describe "GET #create" do
+     it "returns http success" do
+       get :create
+       expect(response).to have_http_status(:success)
+     end
+   end
 
 end
