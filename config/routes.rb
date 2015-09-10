24dea490cb763bbf8be5061a3ccd5f5e4e7c9690
Your Name <joe.scalera@gmail.com>
2015-09-10 06:17:49 -0700

    master clean up

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
   resources :topics do
 
-  end
+
      resources :posts, except: [:index]
   resources :advertisements
+end
   root to: 'welcome#index'
+
   end
