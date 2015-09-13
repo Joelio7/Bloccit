c470126c261d4bc03febe45f8623df2937703a89
Your Name <joe.scalera@gmail.com>
2015-09-12 20:09:54 -0700

    validation hw

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Topic, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData. random_paragraph) }
   it { should have_many(:posts) }
   it {should have_many(:sponsored_posts)}
-  
+
   describe "attributes" do
     it "should respond to name" do
       expect(topic).to respond_to(:name)
@@ -22,5 +22,10 @@ RSpec.describe Topic, type: :model do
     it "should be public by default" do
       expect(topic.public).to be(true)
     end
+    it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:description) }
+
+  it { should validate_length_of(:name).is_at_least(5) }
+  it { should validate_length_of(:description).is_at_least(15) }
   end
 end
