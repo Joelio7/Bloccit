2618aa6d6f886deff53cafa710035bd995301446
Your Name <joe.scalera@gmail.com>
2015-09-22 10:21:09 -0700

    auth sign up hw pt2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913031425) do
+ActiveRecord::Schema.define(version: 20150920053236) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -21,14 +21,14 @@ ActiveRecord::Schema.define(version: 20150913031425) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "answers", force: :cascade do |t|
+  create_table "answer", force: :cascade do |t|
     t.text     "body"
     t.integer  "Question_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  add_index "answers", ["Question_id"], name: "index_answers_on_Question_id"
+  add_index "answer", ["Question_id"], name: "index_answer_on_Question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -45,9 +45,11 @@ ActiveRecord::Schema.define(version: 20150913031425) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
