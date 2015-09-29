bd3bfb330f1782c5d4f1c965c760e3e947afaed3
Your Name <joe.scalera@gmail.com>
2015-09-29 15:18:30 -0700

    rating commit

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,21 +1,14 @@
 class Rating < ActiveRecord::Base
   has_many :topics
   has_many :posts
-  belongs_to :labelable, polymorphic: true
+  belongs_to :rateable, polymorphic: true
 
-  def self.update_rating(rating_string)
-    new_strings = []
-
-    unless rating_string.nil? || rating_string.empty?
-
-      rating_string.split(",").each do |rating|
-        rating_name = rating.strip
-        new_rating = Rating.find_or_create_by(name: rating_name)
-        new_ratings << new_rating
-      end
-    end
-
-    new_rating
-    end
   enum severity: [ :PG, :PG13, :R ]
+
+  def self.update_rating(rating)
+    Rating.find_or_create_by(severity: rating.to_i)
+  end
+    new_rating
+  end
+
 end
