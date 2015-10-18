a86d7596cf17a9c22d9be9c5fdb05cff5cf574df
Your Name <joe.scalera@gmail.com>
2015-10-17 22:13:10 -0700

    token errors

--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,11 @@ resources :questions
 
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,14 +6,13 @@ RSpec.describe User, type: :model do
   it { should have_many(:comments) }
   it { should validate_presence_of(:name) }
   it { should validate_length_of(:name).is_at_least(1) }
-    #shoulda tests for email
+
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
   it { should validate_length_of(:email).is_at_least(3) }
   it {should allow_value("user@bloccit.com").for(:email) }
   it { should_not allow_value("userbloccit.com").for(:email) }
 
-    #shoulda tests for password
   it { should validate_presence_of(:password) }
   it { should have_secure_password }
   it {should validate_length_of(:password).is_at_least(6) }
@@ -44,95 +43,97 @@ RSpec.describe User, type: :model do
      let(:user_with_invalid_email_format) { build(:user, email: "invalid_format") }
 
 # #
-#     it "should be invalid user due to blank name" do
-#       expect(user_with_invalid_name).to_not be_valid
-#     end
-#
-#     it "should be invalid user due to blank email" do
-#       expect(user_with_invalid_email).to_not be_valid
-#     end
-#
-#     it "should be an invalid user due to incorrectly formatted email address" do
-#       expect(user_with_invalid_email_format).to_not be_valid
-#     end
-#   end
-#
-#
-#
-#   it "should respond to role" do
-#      expect(user).to respond_to(:role)
-#    end
-#
-# # #2
-#    it "should respond to admin?" do
-#      expect(user).to respond_to(:admin?)
-#    end
-#
-# # #3
-#    it "should respond to member?" do
-#      expect(user).to respond_to(:member?)
-#    end
-#
-#    it "should respond to moderator?" do
-#      expect(user).to respond_to(:moderator)
-#    end
-#  end
-#
-#  describe "roles" do
-# # #4
-#    it "should be member by default" do
-#      expect(user.role).to eql("member")
-#    end
-#
-# # #5
-#    context "member user" do
-#      it "should return true for #member?" do
-#        expect(user.member?).to be_truthy
-#      end
-#
-#      it "should return false for #admin?" do
-#        expect(user.admin?).to be_falsey
-#      end
-#
-#      it "should return false for #moderator?"
-#       expect(user.moderator?).to be_falsey
-#    end
-#
-# # #6
-#    context "admin user" do
-#      before do
-#        user.admin!
-#      end
-#
-#      it "should return false for #member?" do
-#        expect(user.member?).to be_falsey
-#      end
-#
-#      it "should return true for #admin?" do
-#        expect(user.admin?).to be_truthy
-#      end
-#
-#      it "should return false for #moderator?" do
-#        expect(user.moderator?).to be_falsey
-#      end
-#
-#         context "moderator" do
-#           before do
-#             user.moderator!
-#           end
-#
-#           it "should return false for #admin?" do
-#             expect(user.admin?).to be_falsey
-#           end
-#
-#           it "should return false for #member?" do
-#             expect(user.member?).to be_falsey
-#           end
-#
-#           it "should return true for #moderator?" do
-#             expect(user.moderator).to be_truthy
-#           end
-#         end
+    it "should be invalid user due to blank name" do
+      expect(user_with_invalid_name).to_not be_valid
+    end
+
+    it "should be invalid user due to blank email" do
+      expect(user_with_invalid_email).to_not be_valid
+    end
+
+    it "should be an invalid user due to incorrectly formatted email address" do
+      expect(user_with_invalid_email_format).to_not be_valid
+    end
+  end
+
+
+
+  it "should respond to role" do
+     expect(user).to respond_to(:role)
+   end
+
+# #2
+   it "should respond to admin?" do
+     expect(user).to respond_to(:admin?)
+   end
+
+# #3
+   it "should respond to member?" do
+     expect(user).to respond_to(:member?)
+   end
+
+   it "should respond to moderator?" do
+     expect(user).to respond_to(:moderator?)
+   end
+ end
+
+ describe "roles" do
+# #4
+   it "should be member by default" do
+     expect(user.role).to eql("member")
+   end
+
+# #5
+   context "member user" do
+     it "should return true for #member?" do
+       expect(user.member?).to be_truthy
+     end
+
+     it "should return false for #admin?" do
+       expect(user.admin?).to be_falsey
+     end
+
+     it "should return false for #moderator?"
+      expect(user.moderator?).to be_falsey
+   end
+ end
+# #6
+   context "admin user" do
+     before do
+       user.admin!
+     end
+
+
+     it "should return false for #member?" do
+       expect(user.member?).to be_falsey
+     end
+
+     it "should return true for #admin?" do
+       expect(user.admin?).to be_truthy
+     end
+
+     it "should return false for #moderator?" do
+       expect(user.moderator?).to be_falsey
+     end
+
+
+        context "moderator" do
+          before do
+            user.moderator!
+          end
+
+          it "should return false for #admin?" do
+            expect(user.admin?).to be_falsey
+          end
+
+          it "should return false for #member?" do
+            expect(user.member?).to be_falsey
+          end
+
+          it "should return true for #moderator?" do
+            expect(user.moderator).to be_truthy
+          end
+        end
 
         describe ".avatar_url" do
  # #6
@@ -146,4 +147,9 @@ RSpec.describe User, type: :model do
      end
    end
 
- end
+   describe "#generate_auth_token" do
+        it "creates a token" do
+          expect(user.auth_token).to_not be_nil
+        end
+      end
+    end
