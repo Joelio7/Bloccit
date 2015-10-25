9b8e26f903b22f52cf402decb9ab13679a1c0d27
Your Name <joe.scalera@gmail.com>
2015-10-24 18:21:30 -0700

    API 2 hw

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,4 +9,21 @@ class Api::V1::PostsController < Api::V1::BaseController
     post = Post.find(params[:id])
     render json: post.to_json(include: :comments), status: 200
   end
+
+  def create
+  end
+
+  def update
+    post = Post.find(params[:id])
+    if post.update_attributes(post_params)
+      render json: post.to_json, status: 200
+    else
+      render json: {error: "Post update failed", status: 400}, status: 400
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :public)
+  end
 end
